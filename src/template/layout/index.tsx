b2c6de19522d018131cc576578107236e16533f0
jenish-kilroy <jenish@kilroyblockchain.com>
2022-11-13 22:20:41 +0545

    #7: Implement Header

--- src/template/layout/index.tsx
+++ src/template/layout/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { LayoutType } from "../../core/enum/layout.type";
+import Header from "../header";
 
 const Layout = ({ metaInfo, children }: LayoutType) => {
     return(
@@ -10,6 +11,7 @@ const Layout = ({ metaInfo, children }: LayoutType) => {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <>
+        <Header></Header>
         {children}
         </>
       </>
